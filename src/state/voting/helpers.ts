b52255c671ae5eaac3b1226167ec4b7601e9f624
footballnft <85647902+footballnft@users.noreply.github.com>
2022-06-06 23:18:16 +0100

    third commit

--- src/state/voting/helpers.ts
+++ src/state/voting/helpers.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-restricted-syntax */
 import { SNAPSHOT_API } from 'config/constants/endpoints'
 import request, { gql } from 'graphql-request'
-import { getVotingPowerByCakeStrategy } from 'views/Voting/helpers'
+import { getVotingPowerByCakeStrategy } from 'views/Buy/helpers'
 import { Proposal, ProposalState, Vote, VoteWhere } from 'state/types'
 import _chunk from 'lodash/chunk'
 import _flatten from 'lodash/flatten'
